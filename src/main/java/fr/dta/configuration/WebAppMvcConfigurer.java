49dbd33b7aa855fc46f10e521ea246d31a2636ef
amicius <admin@amicius.fr>
2019-02-07 16:49:14 +0100

    34

--- src/main/java/fr/dta/configuration/WebAppMvcConfigurer.java
+++ src/main/java/fr/dta/configuration/WebAppMvcConfigurer.java
@@ -2,6 +2,7 @@ package fr.dta.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -30,5 +31,12 @@ public class WebAppMvcConfigurer extends WebMvcConfigurerAdapter {
         viewResolver.setPrefix( "/WEB-INF/jsp/" );
         viewResolver.setSuffix( ".jsp" );
         return viewResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename( "i18n/Messages" );
+        return source;
     }
 }
